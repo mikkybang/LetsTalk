0f568163bed10f967b95e6cc0f43cef8ebce05b7
Michael Efemena <mikkybang@localhost.localdomain>
2020-07-04 23:03:50 +0100

    Move config.go to values folder

--- values/config.go
+++ values/config.go
@@ -1,4 +1,4 @@
-package model
+package values
 
 import (
 	"encoding/json"
@@ -11,14 +11,12 @@ var Config Configuration
 func LoadConfiguration() error {
 	file, err := os.Open("../config.json") // For read access.
 	if err != nil {
-		log.Fatal("Error loading the config file", err)
 		return err
 	}
 	defer file.Close()
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&Config)
 	if err != nil {
-		log.Fatal("can't decode config JSON: ", err)
 		return err
 	}
 	return nil
